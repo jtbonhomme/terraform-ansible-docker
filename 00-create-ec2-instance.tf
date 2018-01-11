fe02ec359b4c93f94743170b0b006f4cc63620c9
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2018-01-11 16:15:49 +0100

    Last fixes

--- 00-create-ec2-instance.tf
+++ 00-create-ec2-instance.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "dep-ec2-sg" {
   }
   ingress {
     from_port       = 81
-    to_port         = 22
+    to_port         = 81
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
--- 01-create-inv.tf
+++ 01-create-inv.tf
@@ -7,6 +7,9 @@ resource "null_resource" "ansible-provision" {
     command =  "echo \"[ec2-master]\" > ansible/inventories/test"
   }
   provisioner "local-exec" {
-    command =  "echo \"\n${join("\n",formatlist("%s ansible_ssh_host=%s", aws_instance.ec2-master.tags.Name, aws_instance.ec2-master.public_ip))}\" >> ansible/inventories/test"
+    command =  "echo \"\n${format("%s ansible_ssh_host=%s", aws_instance.ec2-master.tags.Name, aws_instance.ec2-master.public_ip)}\" >> ansible/inventories/test"
   }
-
+  provisioner "local-exec" {
+    command =  "echo \"${format("EC2 instance: %s host public IP: %s", aws_instance.ec2-master.tags.Name, aws_instance.ec2-master.public_ip)}\" "
+  }
+}
