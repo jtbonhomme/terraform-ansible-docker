145b7d3e1b5c35c43095b158f22cae7d895c22c2
Martin Brook <mbrook@jaguarlandrover.com>
2020-05-23 19:27:58 +0100

    Update terraform to new format, increase disk size, remove port 81 ingress

--- 00-create-ec2-instance.tf
+++ 00-create-ec2-instance.tf
@@ -1,51 +1,61 @@
 ##Setup variables
 
-variable "ami" {}
-variable "image-flavor" {}
-variable "key-pair" {}
-variable "aws-region" {}
-variable "tag-name" {}
-variable "tag-cpaccount" {}
+variable "ami" {
+}
+
+variable "image-flavor" {
+}
+
+variable "key-pair" {
+}
+
+variable "aws-region" {
+}
+
+variable "tag-name" {
+}
+
+variable "tag-cpaccount" {
+}
 
 ##Define AWS provider
 provider "aws" {
-  region            = "${var.aws-region}"
+  region = var.aws-region
 }
 
 ##Create a security group for deployment.
 resource "aws_security_group" "dep-ec2-sg" {
-  name              = "deployment-ec2-sg"
-  description       = "Allow incoming ssh and htp connexion on port 81 from anywhere, and any outgoing connexion to anywhere"
+  name        = "deployment-ec2-sg"
+  description = "Allow incoming ssh and htp connexion on port 81 from anywhere, and any outgoing connexion to anywhere"
   ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port       = 81
-    to_port         = 81
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 ##Create masters instances
 resource "aws_instance" "ec2-master" {
-  ami               = "${var.ami}"
-  instance_type     = "${var.image-flavor}"
-  key_name          = "${var.key-pair}"
-  vpc_security_group_ids = ["${aws_security_group.dep-ec2-sg.id}"]
+  ami                         = var.ami
+  instance_type               = var.image-flavor
+  key_name                    = var.key-pair
+  vpc_security_group_ids      = [aws_security_group.dep-ec2-sg.id]
   associate_public_ip_address = true
 
-  tags {
-    Name            = "${var.tag-name}-ec2-master-${timestamp()}"
-    "C+Account"     = "${var.tag-cpaccount}"
+  root_block_device {
+        volume_size = 160
+  }
+
+  tags = {
+    Name        = replace("${var.tag-name}-ec2-master-${timestamp()}","-","_")
+    "C+Account" = var.tag-cpaccount
   }
 }
+
