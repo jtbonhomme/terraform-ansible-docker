194d1debeecd546bd0fd1b72bdf47c14595cc619
Martin Brook <mbrook@jaguarlandrover.com>
2020-05-23 19:28:50 +0100

    Update terraform to new format

--- 01-create-inv.tf
+++ 01-create-inv.tf
@@ -1,15 +1,24 @@
 resource "null_resource" "ansible-provision" {
-
-  depends_on = ["aws_instance.ec2-master"]
+  depends_on = [aws_instance.ec2-master]
 
   ##Create Masters Inventory
+  ##Create Masters Inventory
   provisioner "local-exec" {
-    command =  "echo \"[ec2-master]\" > ansible/inventories/test"
+    command = "echo \"[ec2-master]\" > ansible/inventories/test"
   }
   provisioner "local-exec" {
-    command =  "echo \"\n${format("%s ansible_ssh_host=%s", aws_instance.ec2-master.tags.Name, aws_instance.ec2-master.public_ip)}\" >> ansible/inventories/test"
+    command = "echo \"\n${format(
+      "%s ansible_ssh_host=%s",
+      aws_instance.ec2-master.tags.Name,
+      aws_instance.ec2-master.public_ip,
+    )}\" >> ansible/inventories/test"
   }
   provisioner "local-exec" {
-    command =  "echo \"${format("EC2 instance: %s host public IP: %s", aws_instance.ec2-master.tags.Name, aws_instance.ec2-master.public_ip)}\" "
+    command = "echo \"${format(
+      "EC2 instance: %s host public IP: %s",
+      aws_instance.ec2-master.tags.Name,
+      aws_instance.ec2-master.public_ip,
+    )}\" "
   }
 }
+
